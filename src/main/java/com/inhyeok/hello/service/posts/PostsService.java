f65a90d8096be1051c6144616a8459a5c736cdba
inhyeok <v7153623@naver.com>
2022-03-16 13:46:17 +0900

    220316 수업시간 끝

---
+++ src/main/java/com/inhyeok/hello/service/posts/PostsService.java
@@ -0,0 +1,17 @@
+package com.inhyeok.hello.service.posts;
+import com.inhyeok.hello.domain.posts.PostsRepository;
+import com.inhyeok.hello.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
