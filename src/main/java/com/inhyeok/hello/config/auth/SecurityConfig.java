59857701956190054d3e5593eb388762349fcafa
inhyeok <v7153623@naver.com>
2022-04-18 21:30:21 +0900

    210418 5.5까지 수업 끝 5.6 5.7마치는게 숙제

---
+++ src/main/java/com/inhyeok/hello/config/auth/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.inhyeok.hello.config.auth;
+import com.inhyeok.hello.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.userinfo.CustomUserTypesOAuth2UserService;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile")
+                .permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+    }
+}
--- src/main/java/com/inhyeok/hello/web/IndexController.java
+++ src/main/java/com/inhyeok/hello/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.inhyeok.hello.web;
 
+import com.inhyeok.hello.config.auth.LoginUser;
+import com.inhyeok.hello.config.auth.dto.SessionUser;
 import com.inhyeok.hello.service.posts.PostsService;
 import com.inhyeok.hello.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +10,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
+        if(user!=null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
